627a9010a64da66108b8e0a87ce462062787dad4
vipul906 <vipuljain906@gmail.com>
2020-08-04 13:03:31 +0530

    Updates

---
+++ precache-manifest.b34adb8ceaa3d7b2eb553573dd1ec63e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d07bbea0cd849ff258b8a0e54523f2e",
+    "url": "/budgetapp/index.html"
+  },
+  {
+    "revision": "4d7ae11ff52d84dfbff6",
+    "url": "/budgetapp/static/css/main.d1552f98.chunk.css"
+  },
+  {
+    "revision": "ac0350c3137dde3e8f8e",
+    "url": "/budgetapp/static/js/2.605c2d53.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/budgetapp/static/js/2.605c2d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d7ae11ff52d84dfbff6",
+    "url": "/budgetapp/static/js/main.388524f7.chunk.js"
+  },
+  {
+    "revision": "47ddb75a918b79bdb174",
+    "url": "/budgetapp/static/js/runtime-main.86937b99.js"
+  }
+]);
